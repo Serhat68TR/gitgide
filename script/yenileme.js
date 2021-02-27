c607750d1a49c254fec00316bb0dffb7e56d8b22
Serhat68TR <62091884+Serhat68TR@users.noreply.github.com>
2021-02-27 12:34:56 +0300

    Add files via upload

--- script/yenileme.js
+++ script/yenileme.js
@@ -1,3 +1,6 @@
 if (window.location.href.substr(-2) !== '?r') {
     window.location = window.location.href + '?r';
 }
+if (window.location.href.substr(-2) !== '?s') {
+    window.location = window.location.href + '?s';
+}
