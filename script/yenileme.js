e8c7d32f27607cc4f20084fd4f9210587dc258b5
Serhat68TR <62091884+Serhat68TR@users.noreply.github.com>
2021-02-27 12:14:52 +0300

    Add files via upload

--- script/yenileme.js
+++ script/yenileme.js
@@ -1,1 +1,15 @@
-window.onload = function () {window.location.reload()}
+function reloadPage() {
+    // The last "domLoading" Time //
+    var currentDocumentTimestamp =
+    new Date(performance.timing.domLoading).getTime();
+    // Current Time //
+    var now = Date.now();
+    // Ten Seconds //
+    var tenSec = 10 * 1000;
+    // Plus Ten Seconds //
+    var plusTenSec = currentDocumentTimestamp + tenSec;
+    if (now > plusTenSec) {
+    location.reload();
+    } else {}
+    }
+    reloadPage();
